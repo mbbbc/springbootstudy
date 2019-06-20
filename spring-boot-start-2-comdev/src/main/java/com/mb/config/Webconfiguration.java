3c55974e3d0f464c1c91b9d8ff0ff9960b4b55c1
mobinchao <mobinchao@WIN-TDG2VUSK3KC>
2019-06-20 15:16:59 +0800

    往spring容器添加自定义Filter组件

---
+++ spring-boot-start-2-comdev/src/main/java/com/mb/config/Webconfiguration.java
@@ -0,0 +1,46 @@
+package com.mb.config;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Webconfiguration {
+	
+	@Bean
+	public FilterRegistrationBean<MyFilter> testFilterRegistrationBean() {
+		
+		FilterRegistrationBean<MyFilter> registrationBean = new FilterRegistrationBean<MyFilter>();
+		registrationBean.setFilter(new MyFilter());
+		registrationBean.addUrlPatterns("/*");
+		registrationBean.addInitParameter("paramName", "paramValue");
+		registrationBean.setName("MyFilter");
+		registrationBean.setOrder(1);
+		return registrationBean;
+	}
+	
+	
+	public class MyFilter implements Filter{
+
+		@Override
+		public void doFilter(ServletRequest srequest, ServletResponse sresponse, FilterChain chain)
+				throws IOException, ServletException {
+
+			HttpServletRequest request = (HttpServletRequest) srequest;
+			System.out.println("this is MyFilter, url:" + request.getRequestURI());
+			
+			//放行
+			chain.doFilter(srequest, sresponse);
+		}
+		
+	}
+}

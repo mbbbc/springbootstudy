f1bd4852be3b93376869dea9e3a24c1a42e575f6
mobinchao <mobinchao@WIN-TDG2VUSK3KC>
2019-06-20 15:15:45 +0800

    Spring Boot(二)：Web 综合开发

---
+++ spring-boot-start-2-comdev/src/main/java/com/mb/SpringBootStart2ComdevApplication.java
@@ -0,0 +1,13 @@
+package com.mb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStart2ComdevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStart2ComdevApplication.class, args);
+	}
+
+}

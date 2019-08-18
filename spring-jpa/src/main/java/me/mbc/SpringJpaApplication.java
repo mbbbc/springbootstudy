9f4510d3d6e156bede49fbc6f779f356a3794b57
mbc <mbbbc@qq.com>
2019-08-19 00:55:15 +0800

    jpa懒加载

--- spring-jpa/src/main/java/me/mbc/SpringJpaApplication.java
+++ spring-jpa/src/main/java/me/mbc/SpringJpaApplication.java
@@ -2,6 +2,8 @@ package me.mbc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.orm.jpa.support.OpenEntityManagerInViewFilter;
 
 @SpringBootApplication
 public class SpringJpaApplication {
